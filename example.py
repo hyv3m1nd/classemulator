474ea2889f4b61626ea5871bc441b8edfe6a315a
hyv3m1nd <87497849+hyv3m1nd@users.noreply.github.com>
2023-04-27 19:03:22 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import json
 data_to_print = {"speaker": "Jonathan", "message": "Hello!"}
 
 dumps_output_without_indent = json.dumps(data_to_print)
-print("By default, json.dumps() returns the following:")
+print("By default, json.dumps(data) returns the following:")
 print(dumps_output_without_indent)
 print("")
 
@@ -17,7 +17,7 @@ print("")
 json_emulator_with_indent = ClassEmulator(json, indent=4)
 emulated_dumps_output = json_emulator_with_indent.dumps(data_to_print)
 print(
-    "Using an emulator that stores indent=4, we can get dumps() to return the following without passing indent=4 into dumps() directly:"
+    "Using an emulator that stores indent=4, we can get emulator.dumps(data) to return the following without passing indent=4 into dumps() directly:"
 )
 print(emulated_dumps_output)
 print("")
@@ -27,13 +27,13 @@ json_emulator_with_indent.save_params(
 )
 emulated_dumps_output = json_emulator_with_indent.dumps(data_to_print)
 print(
-    "Also, any attribute that json.dumps() cannot accept is disregarded. For example, passing garbage_parameter yields the following:"
+    "Also, any attribute that json.dumps() cannot accept is disregarded. For example, if emulator has stored garbage_parameter, emulator.dumps(data) yields the following:"
 )
 print(emulated_dumps_output)
 print("")
 
 print(
-    "Note that this does not prevent json.dumps() from throwing an error if you pass an unacceptable parameter directly into emulator.dumps():"
+    "Note that this does not prevent json.dumps(data) from throwing an error if you pass an unacceptable parameter directly into emulator.dumps():"
 )
 json_emulator_with_indent.dumps(
     data_to_print, garbage_parameter="I will break your code!"
