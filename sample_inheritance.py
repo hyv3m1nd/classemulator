93d259abe9ce35854509cbc6c3c17f5239d04d8f
hyv3m1nd <87497849+hyv3m1nd@users.noreply.github.com>
2023-04-29 04:11:43 -0400

    Update sample_inheritance.py

--- sample_inheritance.py
+++ sample_inheritance.py
@@ -1,31 +1,31 @@
 from class_emulator import ClassEmulator
-import emulated_library
+import sste_common
 from typing import Union, List
 
 
-class EmulatorForSpecificLibrary(ClassEmulator):
+class SsteCommonEmulator(ClassEmulator):
     def __init__(self, **kwargs):
-        super().__init__(emulated_library, **kwargs)
+        super().__init__(sste_common, **kwargs)
 
 
     def exec_commands(self, commands: Union[list, str, dict], **kwargs):
         """
-        The emulated library has an exec_commands function, which takes the following arguments:
-        args: a dictionary that must have "commands", which must be a list of strings,
-        other arguments that are stored in the emulator
+        Prerequisites: 
+        1. script_args must be saved as a parameter.
+        1. script_args['uut'] must be set using script_args['uut'] = sste_common._get_connection(devicename=...)
         """
         if isinstance(commands, str):
             commands = [commands]
         if isinstance(commands, list):
             commands = {
-                "commands": commands
+                "sste_commands": commands
             }
         return super().__getattr__("exec_commands")(commands, **kwargs)
     
     
     def __getattr__(self, function_name):
         """
-        Current hypothesis: This needs to be redefined below any function definition, or it would overwrite the functions defined
+        Current hypothesis: This needs to be redefined below any function definition, or it would overwrite the functions above
         # TODO work out how exactly this works with any overwritten function
         """
         return super().__getattr__(function_name)
