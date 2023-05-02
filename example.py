4a9308260aaf19a7a7b29f54e6dfa6a8d2b67f68
hyv3m1nd <87497849+hyv3m1nd@users.noreply.github.com>
2023-05-02 10:51:04 -0400

    Added @classmethod

--- example.py
+++ example.py
@@ -5,12 +5,36 @@ import json
 
 data_to_print = {"speaker": "Jonathan", "message": "Hello!"}
 
+print("We start with this data:")
+print(data_to_print)
+print("")
+
+
+class ClassMethodTester:
+    @classmethod
+    def retrieve(cls, data, key):
+        return data[key]
+
+
+class_method_tester_emulator = ClassEmulator(ClassMethodTester)
+data_retrieved = class_method_tester_emulator.retrieve(data_to_print, "speaker")
+print(
+    "ClassEmulator can handle @classmethod and run the @classmethod retrieve('speaker') function:"
+)
+print(f"emulator.retrieve(data, 'speaker') -> {data_retrieved}")
+print("")
+
 print_emulator = ClassEmulator(simple_printer)
-print("We can emulate a python file that is imported directly and use print_data(data) to get the results:")
+print(
+    "We can emulate a file with no classes and use its print(data) function to get the results:"
+)
 print_emulator.print_data(data_to_print)
+print("")
 
 dumps_output_without_indent = json.dumps(data_to_print)
-print("By default, json.dumps(data) returns the following:")
+print(
+    "Next, we will emulate the json library. By default, json.dumps(data) returns the following:"
+)
 print(dumps_output_without_indent)
 print("")
 
@@ -22,23 +46,23 @@ print("")
 json_emulator_with_indent = ClassEmulator(json, indent=4)
 emulated_dumps_output = json_emulator_with_indent.dumps(data_to_print)
 print(
-    "Using an emulator that stores indent=4, we can get emulator.dumps(data) to return the following without passing indent=4 into dumps() directly:"
+    "Using an emulator that stores indent=4, we can get dumps(data) to return the following without passing indent=4 into dumps() directly:"
 )
 print(emulated_dumps_output)
 print("")
 
-json_emulator_with_indent.save_params(
+json_emulator_with_indent.set_default_params(
     garbage_parameter="I will break your code!"
 )
 emulated_dumps_output = json_emulator_with_indent.dumps(data_to_print)
 print(
-    "Also, any attribute that json.dumps() cannot accept is disregarded. For example, if emulator has stored garbage_parameter, emulator.dumps(data) yields the following:"
+    "Also, any attribute that json.dumps() cannot accept is disregarded. For example, passing garbage_parameter yields the following:"
 )
 print(emulated_dumps_output)
 print("")
 
 print(
-    "Note that this does not prevent json.dumps(data) from throwing an error if you pass an unacceptable parameter directly into emulator.dumps():"
+    "Note that this does not prevent json.dumps() from throwing an error if you pass an unacceptable parameter directly into emulator.dumps():"
 )
 json_emulator_with_indent.dumps(
     data_to_print, garbage_parameter="I will break your code!"
